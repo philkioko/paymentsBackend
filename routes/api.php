1dd059ea0a7777ec0e029b2f2025c57d4c4bc0d7
Phil Kioko <kiokophil@gmail.com>
2019-05-04 18:50:54 +0300

    updated api file for secure routes with jwt

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,12 @@ Route::post('register', 'AuthenticationController@RegisterUser');
 Route::post('/AddItem', 'ItemsController@AddItems');
 Route::post('/ProcessPayments', 'ItemsController@PaymentProcessing');
 
-// Route::group(['middleware' => 'jwt.auth'], function () {
-//     Route::get('AllCustomers', 'AuthenticationController@getAllCustomers');
-// });
+ Route::group(['middleware' => 'jwt.auth'], function () {
+    Route::get('AllCustomers', 'AuthenticationController@getAllCustomers');
+    Route::get('RequestPurchasedItems/{id}', 'ItemsController@GetPurchasedItems');
+    Route::post('DeletePurchasedItem', 'ItemsController@DeleteItemsPurchased');
+    Route::post('QueryDailyReport', 'ReportsController@QueryDailyreport');
+    Route::post('QueryWeeklyReport', 'ReportsController@QueryWeeklyReports');
+    Route::post('QueryMonthlyReport', 'ReportsController@QueryMonthlyReports');
+ });
 
-//SECURING THE GROUP MIDDLEWARE HAD SOME ISSUES, DUE TO TIME I HAVE LEFT IT OUT BUT IT CAN BE CORRECTED LATER. USED JWT LIBRARY FOR AUTHENTICATION
-
-Route::get('AllCustomers', 'AuthenticationController@getAllCustomers');
-Route::get('RequestPurchasedItems/{id}', 'ItemsController@GetPurchasedItems');
-Route::post('DeletePurchasedItem', 'ItemsController@DeleteItemsPurchased');
-Route::post('QueryDailyReport', 'ReportsController@QueryDailyreport');
-Route::post('QueryWeeklyReport', 'ReportsController@QueryWeeklyReports');
-Route::post('QueryMonthlyReport', 'ReportsController@QueryMonthlyReports');
